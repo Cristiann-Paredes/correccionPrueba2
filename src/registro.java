007fbb8d6c6affdf704ee16287b07cb207d41a0a
Cristiann-Paredes <paredes30@hotmail.es>
2023-08-24 21:57:17 -0500

    correccion

--- src/registro.java
+++ src/registro.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
-
+//NOMBRE: Cristian Paredes
 
 public class registro extends JFrame{
     private JPanel rootPanel;
